1b4a410718d8cc857de5ae6c0a8438d138e959ca
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2023-08-19 01:59:20 +0700

    update main again

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.api import router as api_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:8081",
+    "http://localhost:8082",
+    "http://localhost:8083",
+    "https://fe-the-best-employees.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
 
 app.include_router(api_router, prefix="/api/v1")
